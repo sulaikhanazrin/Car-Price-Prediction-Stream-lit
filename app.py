e4e92c40ab873923c1d6f899ab8892930bba716c
sulaikhanazrin <84382653+sulaikhanazrin@users.noreply.github.com>
2025-01-21 20:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,13 @@ import streamlit as st
 import pandas as pd
 
 
-
 import pickle
 
 # Correct path to model file
 model_file_path = "model/gradient_boosting_regressor_model.pkl"
-
 # Load model
-try:
-    with open(model_file_path, 'rb') as model_file:
-        model = pickle.load(model_file)
-    print("Model loaded successfully.")
-except FileNotFoundError:
-    print(f"Error: The file at '{model_file_path}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+with open(model_file_path, 'rb') as model_file:
+    model = pickle.load(model_file)
 
 
 
